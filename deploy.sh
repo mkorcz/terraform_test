f9273582038cd06453d4fdd6dbf006dd39686dc4
Maciej Korcz <maciejkorcz1@gmail.com>
2020-05-12 23:20:39 +0200

    logs from container to json

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 SERVICE_NAME="httpd"
 SERVICE_TAG="latest"
 ECR_REPO_URL="875836252453.dkr.ecr.eu-north-1.amazonaws.com/${SERVICE_NAME}"
-
+LOGS_NAME="httpd-app"
+LOGS_PREFIX="stream"
 if [ "$1" = "dockerize" ]; then
 
     aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 875836252453.dkr.ecr.eu-north-1.amazonaws.com
@@ -14,6 +15,11 @@ if [ "$1" = "dockerize" ]; then
 elif [ "$1" = "deploy" ]; then
     terraform init
     terraform apply -var "docker_image_url=${ECR_REPO_URL}:${SERVICE_TAG}"
+    sleep 60
+    aws logs describe-log-streams --log-group-name ${LOGS_NAME} --log-stream-name-prefix ${LOGS_PREFIX} > logs.json
+    LOG_NAME=`python3 get_log_name.py`
+    echo ${LOG_NAME}
+    aws logs get-log-events --log-group-name ${LOGS_NAME} --log-stream-name ${LOG_NAME} > container_logs.json
 elif [ "$1" = "plan" ]; then
     terraform init
     terraform plan -var "docker_image_url=${ECR_REPO_URL}:${SERVICE_TAG}"
